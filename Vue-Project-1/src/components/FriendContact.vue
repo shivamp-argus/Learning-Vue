c14f8e5703d37f7b4fc23f633c0da5d2ae061e39
shivamp-argus <shivamp@argusoft.com>
2023-03-20 14:12:33 +0530

    [Added FriendContact component]

---
+++ Vue-Project-1/src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible?'Hide':'Show' }} details</button>
+        <ul v-if="detailsAreVisible">
+            <li>Phone : {{ friend.phone }}</li>
+            <li>Email : {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            detailsAreVisible:false,
+            friend:{
+                id:'shivam',
+                name:'Shivam Patel',
+                phone:'97325 12355',
+                email:'shivam@argusoft.com'
+            }
+        }
+    },
+    methods:{
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
